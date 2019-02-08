bbf4b76cf8f6fb91e2291be181239817059e9565
Bogdan Negru <bogdan.negru@freshbyteinc.com>
2019-02-08 16:33:06 +0200

    Edit readme and add some useful files.

---
+++ src/components/Demo.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+
+class Demo extends Component {
+
+
+    render() {
+        return(
+            <div>
+                Add code here.
+            </div>
+        );
+    }
+
+
+}
+
+
+export default Demo;
